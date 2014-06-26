82b05695d24a8305850b705266388dc1d877cf74
Raquel Calderon <calderon.rocky@gmail.com>
2014-06-26 11:52:18 -0700

    xrange => range (py3)

--- basic_crawler.py
+++ basic_crawler.py
@@ -6,8 +6,8 @@ max_size = 1000
 url_queue = Queue()
 content_queue = Queue(maxsize=max_size)
 
-parsers = [Parser.Thread(content_queue, url_queue) for i in xrange(num_threads)]
-grabbers = [Grabber.Thread(url_queue, content_queue) for i in xrange(num_threads)]
+parsers = [Parser.Thread(content_queue, url_queue) for i in range(num_threads)]
+grabbers = [Grabber.Thread(url_queue, content_queue) for i in range(num_threads)]
 
 for thread in parsers+grabbers:
     thread.daemon = True
