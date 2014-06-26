f8ebb6ffef96c2bd4ed083301e7d0451e340eff1
Raquel Calderon <calderon.rocky@gmail.com>
2014-06-26 14:20:08 -0700

    edited

--- basic_crawler.py
+++ basic_crawler.py
@@ -7,7 +7,7 @@ url_queue = Queue()
 content_queue = Queue(maxsize=max_size)
 
 parsers = [Parser.Thread(content_queue, url_queue) for i in range(num_threads)]
-grabbers = [Grabber.Thread(url_queue, content_queue) for i in range(num_threads)]
+grabbers = [Grabber.Thread(url_queue, content_queue) for j in range(num_threads)]
 
 for thread in parsers+grabbers:
     thread.daemon = True
