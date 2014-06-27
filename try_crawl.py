3a7a2cdb770548f9ba566368963d0298db419976
Raquel Calderon <calderon.rocky@gmail.com>
2014-06-27 08:27:50 -0700

    added new crawler source code

---
+++ try_crawl.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = 'http://www.greenseas.eu/content/standards-and-related-web-information'
+
+urls = [url]  # stack of urls to scrape
+visited = [url]  # urls visited
+
+while len(urls) > 0:
+    try:
+        r = requests.get(urls[0])
+        htmlText = r.text
+    except TimeoutError:
+        print('{}: Timeout error'.format(url[0]))
+    soup = BeautifulSoup(htmlText)
+    urls.pop(0)
+
+    for tag in soup.findAll('a', href=True):
+       print (tag)
