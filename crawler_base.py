5fe3ba1d5c491123f164bd733a5fc3a0340ae31d
Anoushka Bose <abose2016@francisparker.org>
2014-07-07 10:06:40 -0700

    Not working version - four different column labels.

--- crawler_base.py
+++ crawler_base.py
@@ -31,7 +31,7 @@ def crawl_links(soup):
             # html_tags.append(tag)
             # mark as visited
             # visited.append(tag['href'])
-    # build_titles(html_tags)
+    # build_labels(html_tags)
     return urls_found
 
 
@@ -77,14 +77,23 @@ def check_link(url):
 
 
 """
-Name: build_titles()
+Name: build_labels()
 Params: url - page to get titles from
 Purpose: Extract the title of the pages these links lead to
 Returns: List of titles
 """
+def build_title(url):
+    var = requests.get(url)
+    html = var.text
+    soup = BeautifulSoup(html)
 
+    title = soup.find('title')
+    if title != None:
+        return title.text
+    else:
+        return " "
 
-def build_titles(soup):
+def build_labels(soup):
     titles_found = []
     for tag in soup.find_all('a', href=True):
         if HTTP in tag['href']:
@@ -145,10 +154,14 @@ first_orgs = [start_org]  # add the base title
 first_titles = [start_title]
 # Use extend function to add all urls and titles found in first run
 first_run.extend(crawl_links(soup))
-first_labels.extend(build_titles(soup))
+first_labels.extend(build_labels(soup))
+
 # not being used as of 7/3/2014 but may be used later
 # second_run = []
 # second_titles = []
+for each in first_run:
+    title = build_title(each)
+    first_titles.append(title)
 
 print('Creating xlsx file')
 # Create excel file
@@ -166,6 +179,7 @@ ws.cell('C1').value = 'URL'
 ws['C1'].style = header_style
 ws.cell('D1').value = 'Organization'
 ws['D1'].style = header_style
+
 max_first = len(first_orgs)
 p = 0
 for row in ws.range('A2:A%s' % max_first):
@@ -200,7 +214,7 @@ for each in first_run:
     hText = (requests.get(each)).text
     crawlSoup = BeautifulSoup(hText)
     linksFound = crawl_links(crawlSoup)  # links found on a page
-    titlesMade = build_titles(crawlSoup)
+    labelsMade = build_labels(crawlSoup)
     # Place the source link above the list of links found
     source_row = ws1.get_highest_row() + 2
     ws1.cell('%s%s' % ('B', source_row)).value = each
@@ -212,7 +226,7 @@ for each in first_run:
         t = 0
         for row in ws1.range('%s%s:%s%s' % ('A', start_row, 'A', last_row)):
             for cell in row:
-                cell.value = titlesMade[t]
+                cell.value = labelsMade[t]
                 t += 1
         k = 0
         for row in ws1.range('%s%s:%s%s' % ('B', start_row, 'B', last_row)):
