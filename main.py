10c2aafeae21bf90cee0dfb60f04f7a62725f39a
Raquel <calderon.rocky@gmail.com>
2014-09-23 00:01:07 -0700

    Can check all links in EC catalog

--- main.py
+++ main.py
@@ -4,29 +4,39 @@ __author__ = 'Raquel'
 
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+from urllib.parse import urljoin
 from check_link import check_link
 
 LAST_PG = 55
 
-# TODO: Add full site capability (all 55 pages)
+base_url = 'http://hydro10.sdsc.edu/'
 
 start_url = 'http://hydro10.sdsc.edu/HLIResources/Resources'
 
-current_page = BeautifulSoup(urlopen(start_url).read())
+soup = BeautifulSoup(urlopen(start_url).read())
+current_page = soup.find('div', {'class': 'pagination'}).find('li', {'class': 'active'})
+page_num = current_page.find('a').text
 
-# Get the table
-table = current_page.find('table')
+while int(page_num) <= LAST_PG:
+    # Get the table
+    table = soup.find('table')
 
-# Get all rows in the table
-table_rows = table.find_all('tr')
+    # Get all rows in the table
+    table_rows = table.find_all('tr')
 
-# Pop off header row
-table_rows.pop(0)
+    # Pop off header row
+    table_rows.pop(0)
 
-# Each row in the table is a resource
-for a_resource in table_rows:
-    title = a_resource.find('td', {'class': 'resTitle'}).find('div').text
-    link_tag = a_resource.find('a', text='Link')
-    status = check_link(link_tag['href'])
-    if status is not "working":
-        print('{}: {}'.format(title, status))
+    # Each row in the table is a resource
+    for a_resource in table_rows:
+        title = a_resource.find('td', {'class': 'resTitle'}).find('div').text
+        link_tag = a_resource.find('a', text='Link')
+        status = check_link(link_tag['href'])
+        if status is not "working":
+            print('{}: {}'.format(title, status))
+
+    next_page = current_page.find_next('li').find('a')
+    full_url = urljoin(base_url, next_page['href'])
+    soup = BeautifulSoup(urlopen(full_url).read())
+    current_page = soup.find('div', {'class': 'pagination'}).find('li', {'class': 'active'})
+    page_num = current_page.find('a').text
