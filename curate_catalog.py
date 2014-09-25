b293e54384b01f69e25fd65678a571aa9da455ad
Raquel <calderon.rocky@gmail.com>
2014-09-25 10:07:33 -0700

    Renamed

--- curate_catalog.py
+++ curate_catalog.py
@@ -7,17 +7,20 @@ from urllib.request import urlopen
 from urllib.parse import urljoin
 from check_link import check_link
 
-LAST_PG = 55
+NO_LINK = 'No link'
+BROKEN_LINK = 'BROKEN LINK'
 
 base_url = 'http://hydro10.sdsc.edu/'
 
 start_url = 'http://hydro10.sdsc.edu/HLIResources/Resources'
 
+
+def already_tagged(res_title):
+    return BROKEN_LINK in res_title
+
 soup = BeautifulSoup(urlopen(start_url).read())
 current_page = soup.find('div', {'class': 'pagination'}).find('li', {'class': 'active'})
-page_num = current_page.find('a').text
-find_next = current_page.find_next('li')
-next_page = find_next.find('a')
+
 
 while current_page.find_next('li') is not None:
     # Get the table
@@ -32,15 +35,16 @@ while current_page.find_next('li') is not None:
     # Each row in the table is a resource
     for a_resource in table_rows:
         title = a_resource.find('td', {'class': 'resTitle'}).find('div').text
-        short_title = (title[:50] + '..') if len(title) > 50 else title
+        short_title = (title[:50] + '..').strip() if len(title) > 50 else title.strip()
         link_tag = a_resource.find('a', href=True, text='Link')
         if link_tag is not None:
             status = check_link(link_tag['href'])
-            if status is not "working":
+            if status is not "working" and not already_tagged(title):
                 print('PAGE {}. {} : {}'.format(page_num, short_title, status))
+        else:
+            ('PAGE {}. {}: {}'.format(page_num, short_title, NO_LINK))
 
     next_page = current_page.find_next('li').find('a')
     full_url = urljoin(base_url, next_page['href'])
     soup = BeautifulSoup(urlopen(full_url).read())
     current_page = soup.find('div', {'class': 'pagination'}).find('li', {'class': 'active'})
-    page_num = current_page.find('a').text
