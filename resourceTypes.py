eb0604d1376a0f3d46b9aeb52cea8d79c05af960
Raquel <calderon.rocky@gmail.com>
2014-12-08 12:37:11 -0800

    Updated resource types

--- resourceTypes.py
+++ resourceTypes.py
@@ -1,18 +1,55 @@
 __author__ = 'Raquel'
 
-resourceTypesKnown = {'Activity': ["Conference"],
-                      'Consensus Effort': ["Consortium", "Association", "Union"],
-                      'Data Service': ["Network", "Services", "Tools", "Platform", "Infrastructure"],
-                      'Catalog': ["search engine", "catalog"], 'Community': ["community"],
-                      'Web Application': ["web application"],
-                      'Portal': ["Visualization data", "Registry", "Infrastructure"],
-                      'Specification': ["specification"],
-                      'Image Collection': ["observation", "images", "gallery", "photography", "picture"],
-                      'Web page': ["web page"],
-                      'Interchange format': ["extension"],
-                      'Vocabulary': ['vocabulary', 'vocab'],
-                      'Service': ["spatial analysis", "spatial mapping"],
-                      'Digital Repository': ["digital repository", "repository"],
+resourceTypesKnown = {'Activity': ['conference', 'meeting', 'project'],
+                      'Catalog': ['database', 'catalog'],
+                      'Collection': ['data portal', 'portal'],
+                      'Community': ["community"],
+                      'Data or Information Resource': ["network", "services", "tools", "platform", "infrastructure",
+                                                       'registry'],
+                      'Data production tool': ['device', 'measurement', 'instrument'],
+                      'Data sensor feed': ['real time', 'real-time'],
+                      'Data set': ['dataset', 'data set'],
+                      'Digital data repository': ['repository', 'source code'],
+                      'Facility': ['observatory', 'ship'],
+                      'Forum': ['forum', 'discussion'],
+                      'Image Collection': ['images', 'photographs', 'gallery'],
+                      'Interchange format': ['markup language'],
+                      'Model' : ['model', 'grid', 'vector', 'graph'],
+                      'Narrative resource': ['document', 'article', 'blog', 'journal'],
+                      'Physical Artifact Collection': ['samples', 'artifacts'],
+                      'Specification': ['specification', 'profile', 'standard'],
+                      'Service resource': ['query', 'web service', 'data service', 'services'],
                       'Functional Specification': ["functional specification", "queries of data"],
-                      'Software': ["software", "code", "programming"],
-                      'Forum': ["forum"], 'Organization': ["organization"]}
+                      'Standalone software': ['desktop application'],
+                      'Software resource': ['software', 'command line tool', 'application'],
+                      'Version Control System': ['VCS', 'version control'],
+                      'Vocabulary': ['vocabulary', 'controlled vocabulary', 'terms'],
+                      'Web Application': ['online tool', 'web application']
+                      }
+"""
+{'SuperCategory:': 'Numeric dataset', 'URL': '', '_id': 'nlx_158021', 'name': 'Gridded numeric data'}
+{'SuperCategory:': 'Data or information resource', 'URL': '', '_id': 'nlx_157898', 'name': 'Identifier mapping resource'}
+{'SuperCategory:': 'Production service resource', 'URL': '', '_id': 'nlx_157906', 'name': 'Identifier resolution'}
+{'SuperCategory:': 'Data or information resource', 'URL': '', '_id': 'birnlex_11005', 'name': 'Image'}
+{'SuperCategory:': 'Model', 'URL': '', '_id': 'nlx_158139', 'name': 'Information model'}
+{'SuperCategory:': 'Feed', 'URL': '', '_id': 'birnlex_2363', 'name': 'Listserv'}
+{'SuperCategory:': 'Image', 'URL': 'http://purl.org/ontology/bibo/Map', '_id': 'nlx_157870', 'name': 'Map'}
+{'SuperCategory:': 'Image', 'URL': 'http://purl.org/dc/dcmitype/MovingImage', '_id': 'nlx_158018', 'name': 'Moving image'}
+{'SuperCategory:': 'Data set', 'URL': '', '_id': 'nlx_158138', 'name': 'Numeric dataset'}
+{'SuperCategory:': 'Object aggregate', 'URL': 'http://purl.obolibrary.org/obo/OBI_0000245', '_id': 'nlx_152328', 'name': 'Organization'}
+{'SuperCategory:': 'Human', 'URL': 'http://semanticscience.org/resource/SIO_000498', '_id': 'SIO_000498', 'name': 'Person'}
+{'SuperCategory:': 'Image', 'URL': 'http://purl.obolibrary.org/obo/IAO_0000185', '_id': 'IAO_0000185', 'name': 'Photograph'}
+{'SuperCategory:': 'Object', 'URL': 'http://purl.org/dc/dcmitype/PhysicalObject', '_id': 'nlx_158026', 'name': 'Physical object'}
+{'SuperCategory:': 'Repository', 'URL': '', '_id': 'nlx_158022', 'name': 'Physical repository'}
+{'SuperCategory:': 'Object aggregate', 'URL': '', '_id': 'birnlex_11030', 'name': 'Platform'}
+{'SuperCategory:': 'Collection', 'URL': '', '_id': 'nlx_158009', 'name': 'Portrayal catalog'}
+{'SuperCategory:': 'Image', 'URL': '', '_id': 'nlx_157865', 'name': 'Remote sensing Earth image'}
+{'SuperCategory:': '', 'URL': 'http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#C16143', '_id': 'C16143', 'name': 'Repository'}
+{'SuperCategory:': 'Resource Descriptor', 'URL': '', '_id': 'nlx_res_20090105', 'name': 'Service resource'}
+{'SuperCategory:': 'Feed', 'URL': '', '_id': 'nlx_157867', 'name': 'Social feed'}
+{'SuperCategory:': '', 'URL': 'http://purl.org/dc/dcmitype/Text', '_id': 'nlx_158019', 'name': 'Text'}
+{'SuperCategory:': 'Numeric dataset', 'URL': 'http://purl.obolibrary.org/obo/IAO_0000584', '_id': 'IAO_0000584', 'name': 'Time sampled measurement data set'}
+{'SuperCategory:': 'Software development tool', 'URL': '', '_id': 'birnlex_2246', 'name': 'Version source control system'}
+{'SuperCategory:': 'Training material', 'URL': 'http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#C42753', '_id': 'C42753', 'name': 'Workflow'}
+{'SuperCategory': 'Still image', '_id': 'nlx_158069', 'name': 'Artwork'}
+"""
